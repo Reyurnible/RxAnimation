bfa75b8208102d52a553fcccb60df8fb7dd73350
Shun Hosaka <reyurnible@gmail.com>
2015-10-08 01:42:33 +0900

    create project

---
+++ rxanimation/src/main/java/com/hosaka/rxanimation/RxAnimation.java
@@ -0,0 +1,17 @@
+package com.hosaka.rxanimation;
+
+import android.support.annotation.CheckResult;
+import android.support.annotation.NonNull;
+
+import java.util.Observable;
+
+/**
+ * Created by shunhosaka on 15/10/08.
+ */
+public final class RxAnimation {
+
+
+
+
+
+}
