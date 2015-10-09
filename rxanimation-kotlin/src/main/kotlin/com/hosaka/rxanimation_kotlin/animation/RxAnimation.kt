6b4f867a79d74058e145b705b78357d5b69f58aa
Shun Hosaka <reyurnible@gmail.com>
2015-10-10 00:57:18 +0900

    add Animator for Kotlin

--- rxanimation-kotlin/src/main/kotlin/com/hosaka/rxanimation_kotlin/animation/RxAnimation.kt
+++ rxanimation-kotlin/src/main/kotlin/com/hosaka/rxanimation_kotlin/animation/RxAnimation.kt
@@ -5,7 +5,4 @@ import com.hosaka.rxanimation.animation.AnimationEvent
 import com.hosaka.rxanimation.animation.RxAnimation
 import rx.Observable
 
-/**
- * Created by shunhosaka on 15/10/09.
- */
 public inline fun Animation.events(): Observable<AnimationEvent> = RxAnimation.events(this)
