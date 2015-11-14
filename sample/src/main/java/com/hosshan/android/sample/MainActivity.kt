08ae7cd469d350aaa2e03e48c41e53ee276a8fa6
Shun Hosaka <reyurnible@gmail.com>
2015-11-14 17:54:37 +0900

    update sample

--- sample/src/main/java/com/hosshan/android/sample/MainActivity.kt
+++ sample/src/main/java/com/hosshan/android/sample/MainActivity.kt
@@ -1,76 +1,80 @@
 package com.hosshan.android.sample
 
-import android.graphics.Color
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import android.view.animation.AlphaAnimation
-import android.view.animation.ScaleAnimation
 import android.widget.Button
+import android.widget.EditText
 import android.widget.RelativeLayout
 import android.widget.TextView
 import butterknife.bindView
 import com.hosaka.rxanimation.animation.AnimationEvent
 import com.hosaka.rxanimation.animator.AnimatorEvent
 import com.hosaka.rxanimation_kotlin.animation.bindView
+import com.hosaka.rxanimation_kotlin.animation.events
 import rx.Observable
-import rx.Subscriber
 
 class MainActivity : AppCompatActivity() {
 
     val loginButton: Button by bindView(R.id.main_button_login)
     val formLayout: RelativeLayout by bindView(R.id.main_layout_form)
+    val mailEditText: EditText by bindView(R.id.main_edittext_mail)
+    val passEditText: EditText by bindView(R.id.main_edittext_pass)
+    val signupTextView: TextView by bindView(R.id.main_textview_signup)
+    val forgotTextView: TextView by bindView(R.id.main_textview_forgot)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         // initialize invisible
         formLayout.visibility = View.INVISIBLE
-
         loginButton.setOnClickListener { startAnimations() }
     }
 
     private fun startAnimations() {
+        mailEditText.visibility = View.INVISIBLE
+        passEditText.visibility = View.INVISIBLE
+        signupTextView.visibility = View.INVISIBLE
+        forgotTextView.visibility = View.INVISIBLE
 
         val alphaAnimation: AlphaAnimation = AlphaAnimation(0.0f, 1.0f)
-        alphaAnimation.duration = 1200
-
-        // Simple Use
-        alphaAnimation.bindView(formLayout).subscribe(object : Subscriber<AnimationEvent>() {
-            override fun onCompleted() {
-
-            }
-
-            override fun onNext(t: AnimationEvent) {
-                when(t.kind()){
-                    AnimationEvent.Kind.START -> {
-                        formLayout.visibility = View.VISIBLE
-                    }
-                    AnimationEvent.Kind.REPEAT -> {
-
-                    }
-                    AnimationEvent.Kind.END -> {
-
-                    }
+        alphaAnimation.duration = 3000
+        val alphaAnimationObserver = alphaAnimation.bindView(formLayout).map {
+            Log.d(MainActivity::class.java.simpleName, it.toString())
+            when (it.kind()) {
+                AnimationEvent.Kind.START -> {
+                    formLayout.visibility = View.VISIBLE
+                    false
                 }
-                Log.d(MainActivity::class.java.simpleName, t.toString())
+                AnimationEvent.Kind.REPEAT -> false
+
+                AnimationEvent.Kind.END -> true
             }
+        }
 
-            override fun onError(e: Throwable?) {
-
+        val translationAnimator: ObjectAnimator = ObjectAnimator.ofFloat(formLayout, "translationY", -1000f, 0f);
+        translationAnimator.setDuration(3000);
+        val translateAnimatorObserver = translationAnimator.events().map {
+            Log.d(MainActivity::class.java.simpleName, it.toString())
+            when (it.kind()) {
+                AnimatorEvent.Kind.START -> false
+                AnimatorEvent.Kind.REPEAT -> false
+                AnimatorEvent.Kind.CANCEL -> false
+                AnimatorEvent.Kind.END -> true
             }
-        })
+        }
 
-        /*val scaleAnimationObserver = scaleAnimation.bindView(textView).map { it.kind() == AnimationEvent.Kind.END }
-        val alphaAnimationObserver = alphaAnimation.bindView(textView).map { it.kind() == AnimationEvent.Kind.END }
-
-        Observable.combineLatest(scaleAnimationObserver, alphaAnimationObserver, { t1, t2 ->
-            (t1 ?: false).and(t2 ?: false)
+        Observable.combineLatest(translateAnimatorObserver, alphaAnimationObserver, { t1, t2 ->
+            t1.and(t2)
         }).subscribe { validate ->
             // post event
-            textView.setTextColor(Color.RED)
-        }*/
+            Log.d(MainActivity::class.java.simpleName, "End All Animation")
+            signupTextView.visibility = View.VISIBLE
+            forgotTextView.visibility = View.VISIBLE
+        }
     }
 
 }
