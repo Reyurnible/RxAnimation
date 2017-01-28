196a9a28b11155a45868dbd8d6c5e8e90e7efafa
Shun Hosaka <reyurnible@gmail.com>
2017-01-28 21:47:32 +0900

    add creater function

--- rxanimation/src/main/java/com/hosaka/rxanimation/animator/RxAnimator.java
+++ rxanimation/src/main/java/com/hosaka/rxanimation/animator/RxAnimator.java
@@ -1,6 +1,7 @@
 package com.hosaka.rxanimation.animator;
 
 import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
 import android.os.Build;
 import android.support.annotation.CheckResult;
@@ -24,6 +25,12 @@ public final class RxAnimator {
     @NonNull
     public static Observable<AnimatorPauseEvent> pauseEvents(@NonNull Animator animation) {
         return Observable.create(new AnimatorPauseEventOnSubscribe(animation));
+    }
+
+    @CheckResult
+    @NonNull
+    public static Observable<AnimatorUpdateEvent> updateEvents(@NonNull ValueAnimator animation) {
+        return Observable.create(new AnimatorUpdateEventOnSubscribe(animation));
     }
 
 }
