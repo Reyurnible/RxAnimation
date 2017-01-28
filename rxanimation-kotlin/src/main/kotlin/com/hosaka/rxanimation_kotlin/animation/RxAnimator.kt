90362858dbf810f58354e69468ebecf66db016c6
Shun Hosaka <reyurnible@gmail.com>
2017-01-28 21:56:23 +0900

    update kotlin extentions

--- rxanimation-kotlin/src/main/kotlin/com/hosaka/rxanimation_kotlin/animation/RxAnimator.kt
+++ rxanimation-kotlin/src/main/kotlin/com/hosaka/rxanimation_kotlin/animation/RxAnimator.kt
@@ -1,8 +1,15 @@
 package com.hosaka.rxanimation_kotlin.animation
 
 import android.animation.Animator
+import android.animation.ValueAnimator
 import com.hosaka.rxanimation.animator.AnimatorEvent
+import com.hosaka.rxanimation.animator.AnimatorPauseEvent
+import com.hosaka.rxanimation.animator.AnimatorUpdateEvent
 import com.hosaka.rxanimation.animator.RxAnimator
 import rx.Observable
 
-fun Animator.bind(): Observable<AnimatorEvent> = RxAnimator.events(this)
+fun Animator.events(): Observable<AnimatorEvent> = RxAnimator.events(this)
+
+fun Animator.pauseEvents(): Observable<AnimatorPauseEvent> = RxAnimator.pauseEvents(this)
+
+fun ValueAnimator.updateEvents(): Observable<AnimatorUpdateEvent> = RxAnimator.updateEvents(this)
